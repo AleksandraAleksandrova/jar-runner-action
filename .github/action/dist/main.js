1f10d63ec3081fb3a70bba8921a80116808c8fa8
AleksandraAleksandrova <a.aleksandrova2004@gmail.com>
2023-08-10 17:31:11 +0300

    switch js for ts

---
+++ .github/action/dist/main.js
@@ -0,0 +1,16 @@
+import * as core from '@actions/core';
+import { exec } from 'child_process';
+try {
+    const jarPath = core.getInput('jar-path');
+    exec(`java -jar ${jarPath}`, (error, stdout, stderr) => {
+        if (error) {
+            core.setFailed(`Error: ${error.message}`);
+            return;
+        }
+        console.log(stdout);
+        core.setOutput('output', stdout);
+    });
+}
+catch (error) {
+    core.setFailed(error.message);
+}
