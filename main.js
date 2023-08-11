3ea59b15860cd21f25f00fe0da9f496e1caa6d4c
AleksandraAleksandrova <a.aleksandrova2004@gmail.com>
2023-08-11 14:15:36 +0300

    use relative path

--- main.js
+++ main.js
@@ -4,18 +4,15 @@ try {
     const path = require('path');
 
     try {
-        console.log('Running JAR file...');
         console.log('Current working directory:', process.cwd());
-        console.log('Current directory content:', exec('ls -la'));
-        console.log('jar path from core input:', core.getInput('jar-path'));
         const jarPathRelative = core.getInput('jar-path').trim();
         console.log('Relative Jar Path from argv:', jarPathRelative);
 
-        // Construct the absolute path to the JAR file
         const jarPathAbsolute = path.resolve(process.cwd(), jarPathRelative);
         console.log('Absolute Jar Path:', jarPathAbsolute);
 
-        exec(`java -jar ${jarPathAbsolute}`, (error, stdout, stderr) => {
+        // use relative path to jar file
+        exec(`java -jar ${jarPathRelative}`, (error, stdout, stderr) => {
             if (error) {
                 core.setFailed(`Error: ${error.message}`);
                 return;
