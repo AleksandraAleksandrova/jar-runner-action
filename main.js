f58ec9d7a236812bf09cd3d73dda1f65bded8610
AleksandraAleksandrova <a.aleksandrova2004@gmail.com>
2023-08-11 12:09:25 +0300

    add try catch around required packages

--- main.js
+++ main.js
@@ -1,16 +1,23 @@
-import * as core from '@actions/core';
-import { exec } from 'child_process';
 try {
-    const jarPath = core.getInput('jar-path');
-    exec(`java -jar ${jarPath}`, (error, stdout, stderr) => {
-        if (error) {
-            core.setFailed(`Error: ${error.message}`);
-            return;
-        }
-        console.log(stdout);
-        core.setOutput('output', stdout);
-    });
-}
-catch (error) {
-    core.setFailed(error.message);
-}
+    const core = require('@actions/core');
+    const { exec } = require('child_process');
+
+    try {
+        const jarPath = core.getInput('jar-path');
+        exec(`java -jar ${jarPath}`, (error, stdout, stderr) => {
+            if (error) {
+                core.setFailed(`Error: ${error.message}`);
+                return;
+            }
+            console.log(stdout);
+            core.setOutput('output', stdout);
+        });
+    }
+    catch (error) {
+        core.setFailed(error.message);
+    }
+
+} catch (error) {
+    console.error('Required package is missing:', error.message);
+    process.exit(1);
+} 
