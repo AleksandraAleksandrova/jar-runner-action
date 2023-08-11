bd47d753b14b64d3157f4d410333ecb005451bc1
AleksandraAleksandrova <a.aleksandrova2004@gmail.com>
2023-08-11 14:47:18 +0300

    setup java

--- main.js
+++ main.js
@@ -1,47 +1,18 @@
 try {
     const core = require('@actions/core');
     const { exec } = require('child_process');
-    const path = require('path');
 
     try {
-        console.log('Running the jar exactly as in my local terminal:')
-        exec('java -jar ./HelloWorld.jar', (error, stdout, stderr) => {
-            console.log('Output:', stdout);
-        });
-
-        console.log('Local run passed.\nCurrent working directory:', process.cwd());
-        
-        console.log("USE RELATIVE PATH")
         const jarPathRelative = core.getInput('jar-path').trim();
-        console.log('Relative Jar Path from workflow:', jarPathRelative);
-        exec(`ls -l ${jarPathRelative}`, (lsError, lsStdout, lsStderr) => {
-            console.log('ls Output:', lsStdout);
-            console.error('ls Error:', lsError);
-
-            exec(`java -jar ${jarPathRelative}`, (error, stdout, stderr) => {
-                if (error) {
-                    core.setFailed(`Error: ${error.message}`);
-                }
-                console.log(stdout);
-                core.setOutput('output', stdout);
-            });
+        console.log(`Running jar: ${jarPathRelative}`);
+        exec(`java -jar ${jarPathRelative}`, (error, stdout, stderr) => {
+            if (error) {
+                core.setFailed(`Error: ${error.message}`);
+            }
+            console.log(stdout);
+            core.setOutput('output', stdout);
         });
 
-        console.log("USE ABSOLUTE PATH")
-        const jarPathAbsolute = path.resolve(process.cwd(), jarPathRelative);
-        console.log('Absolute Jar Path:', jarPathAbsolute);
-        exec(`ls -l ${jarPathAbsolute}`, (lsError, lsStdout, lsStderr) => {
-            console.log('ls Output:', lsStdout);
-            console.error('ls Error:', lsError);
-
-            exec(`java -jar ${jarPathAbsolute}`, (error, stdout, stderr) => {
-                if (error) {
-                    core.setFailed(`Error: ${error.message}`);
-                }
-                console.log(stdout);
-                core.setOutput('output', stdout);
-            });
-        });
     } catch (error) {
         core.setFailed(error.message);
     }
