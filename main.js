9e14408b1391a5064a253a072591aa78f2955c33
AleksandraAleksandrova <a.aleksandrova2004@gmail.com>
2023-08-11 14:12:14 +0300

    debug logs

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
 try {
     const core = require('@actions/core');
     const { exec } = require('child_process');
+    const path = require('path');
 
     try {
-        //const jarPath = core.getInput('jar-path');
+        console.log('Running JAR file...');
         console.log('Current working directory:', process.cwd());
-        const jarPath = process.cwd() + '/HelloWorld.jar';
-        
-        exec(`java -jar ${jarPath}`, (error, stdout, stderr) => {
+        console.log('Current directory content:', exec('ls -la'));
+        console.log('jar path from core input:', core.getInput('jar-path'));
+        const jarPathRelative = core.getInput('jar-path').trim();
+        console.log('Relative Jar Path from argv:', jarPathRelative);
+
+        // Construct the absolute path to the JAR file
+        const jarPathAbsolute = path.resolve(process.cwd(), jarPathRelative);
+        console.log('Absolute Jar Path:', jarPathAbsolute);
+
+        exec(`java -jar ${jarPathAbsolute}`, (error, stdout, stderr) => {
             if (error) {
                 core.setFailed(`Error: ${error.message}`);
                 return;
