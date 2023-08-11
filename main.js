5f127d5a52f188ab6cc79ab3e29f2d0874e57af5
AleksandraAleksandrova <a.aleksandrova2004@gmail.com>
2023-08-11 14:29:35 +0300

    more debugging

--- main.js
+++ main.js
@@ -4,21 +4,49 @@ try {
     const path = require('path');
 
     try {
-        console.log('Current working directory:', process.cwd());
-        const jarPathRelative = core.getInput('jar-path').trim();
-        console.log('Relative Jar Path from argv:', jarPathRelative);
+        console.log('Running the jar exactly as in my local terminal:')
+        exec('java -jar ./HelloWorld.jar', (error, stdout, stderr) => {
+            if (error) 
+                core.setFailed(`Error: ${error.message}`);
+            else 
+                console.log(stdout);
+        });
 
+        console.log('Local run passed \n Current working directory:', process.cwd());
+        
+
+        console.log("USE RELATIVE PATH")
+        const jarPathRelative = core.getInput('jar-path').trim();
+        console.log('Relative Jar Path from workflow:', jarPathRelative);
+        exec(`ls -l ${jarPathRelative}`, (lsError, lsStdout, lsStderr) => {
+            console.log('ls Output:', lsStdout);
+            console.error('ls Error:', lsError);
+
+            exec(`java -jar ${jarPathRelative}`, (error, stdout, stderr) => {
+                if (error) {
+                    core.setFailed(`Error: ${error.message}`);
+                    return;
+                }
+                console.log(stdout);
+                core.setOutput('output', stdout);
+            });
+        });
+
+        console.log("USE ABSOLUTE PATH")
         const jarPathAbsolute = path.resolve(process.cwd(), jarPathRelative);
         console.log('Absolute Jar Path:', jarPathAbsolute);
+        exec(`ls -l ${jarPathAbsolute}`, (lsError, lsStdout, lsStderr) => {
+            console.log('ls Output:', lsStdout);
+            console.error('ls Error:', lsError);
 
-        // use relative path to jar file
-        exec(`java -jar ${jarPathRelative}`, (error, stdout, stderr) => {
-            if (error) {
-                core.setFailed(`Error: ${error.message}`);
-                return;
-            }
-            console.log(stdout);
-            core.setOutput('output', stdout);
+            exec(`java -jar ${jarPathAbsolute}`, (error, stdout, stderr) => {
+                if (error) {
+                    core.setFailed(`Error: ${error.message}`);
+                    return;
+                }
+                console.log(stdout);
+                core.setOutput('output', stdout);
+            });
         });
     } catch (error) {
         core.setFailed(error.message);
