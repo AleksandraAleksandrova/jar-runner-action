35c354fdab14068cb204b358a94cbb9845adbefc
AleksandraAleksandrova <a.aleksandrova2004@gmail.com>
2023-08-14 12:16:28 +0300

    add some kind of auth

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 const core = require('@actions/core');
 const { exec } = require('child_process');
 const axios = require('axios');
+const token = process.env.GITHUB_TOKEN;
 
 try {
   const jarUrl = 'https://github.com/AleksandraAleksandrova/jar-runner-action/releases/download/1.0/HelloWorld.jar';
   axios.get(jarUrl, {
     responseType: 'arraybuffer',
+    headers: {
+        Authorization: `Bearer ${token}`,
+    },
   })
   .then(response => {
     exec(`java -jar HelloWorld.jar`, (error, stdout, stderr) => {
