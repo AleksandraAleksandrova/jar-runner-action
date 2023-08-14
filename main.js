f865773dd9e2f07dd16a84094330f5b95c1541e7
AleksandraAleksandrova <a.aleksandrova2004@gmail.com>
2023-08-14 12:07:12 +0300

    add axios as dependency

--- main.js
+++ main.js
@@ -1,28 +1,3 @@
-try {
-    const core = require('@actions/core');
-    const { exec } = require('child_process');
-
-    try {
-        const jarUrl = 'https://github.com/AleksandraAleksandrova/jar-runner-action/releases/download/1.0/HelloWorld.jar';
-        const jarPathRelative = core.getInput('jar-path').trim();
-        exec(`java -jar ${jarPathRelative}`, (error, stdout, stderr) => {
-            if (error) {
-                core.setFailed(`Error: ${error.message}`);
-            }
-            console.log(stdout);
-            core.setOutput('jar-output', stdout);
-        });
-
-    } catch (error) {
-        core.setFailed(error.message);
-    }
-
-} catch (error) {
-    console.error('Required package is missing:', error.message);
-} 
-
-
-
 const core = require('@actions/core');
 const { exec } = require('child_process');
 const axios = require('axios');
