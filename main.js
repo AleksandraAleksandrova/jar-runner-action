bd95078991a4f78e5203807e0f700ccd3bef9fd6
AleksandraAleksandrova <a.aleksandrova2004@gmail.com>
2023-08-28 12:25:05 +0300

    try using the jar from private repo

--- main.js
+++ main.js
@@ -1,33 +1,15 @@
 const core = require('@actions/core');
-const { exec } = require('child_process');
-const axios = require('axios');
-const token = process.env.GITHUB_TOKEN;
+const exec = require('@actions/exec');
 
-try {
-  const jarUrl = 'https://github.com/AleksandraAleksandrova/jar-runner-action/releases/download/1.0/HelloWorld.jar';
+async function run() {
+  try {
+    const jarPath = './HelloWorld.jar';
 
-  axios.get(jarUrl, {
-    responseType: 'arraybuffer',
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  })
-  .then(response => {
-    const fs = require('fs');
-    fs.writeFileSync('HelloWorld.jar', response.data);
+    await exec.exec('java', ['-jar', jarPath]);
 
-    exec('java -jar HelloWorld.jar', (error, stdout, stderr) => {
-      if (error) {
-        core.setFailed(`Error: ${error.message}`);
-      }
-      console.log(stdout);
-      core.setOutput('jar-output', stdout);
-    });
-  })
-  .catch(error => {
-    core.setFailed(`Error: ${error.message}`);
-  });
-
-} catch (error) {
-  core.setFailed(error.message);
+  } catch (error) {
+    core.setFailed(`Action failed with error: ${error.message}`);
+  }
 }
+
+run();
