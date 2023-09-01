f62a0692e1d411124861e169ee64e17556c25ca7
AleksandraAleksandrova <a.aleksandrova2004@gmail.com>
2023-09-01 12:47:43 +0300

    debug 4

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ const core = require('@actions/core');
 const exec = require('@actions/exec');
 const path = require('path');
 const workingDir = process.cwd();
-console.log(`Working Directory: ${workingDir}`);
 
 async function run() {
   try {
@@ -22,16 +21,19 @@ async function run() {
     };
     */
 
+    console.log(`Working Directory: ${workingDir}`);
+
     console.log(`Jar Path: ${jarPath}`);
     await exec.exec('java', ['-jar', jarPath, name]);
-    //core.setOutput('jar-output', stdout);
+    console.log('Executed Java');
 
     console.log(`Jar Path: ${jarPath1}`);
     await exec.exec('java', ['-jar', jarPath1, name]);
-    //core.setOutput('jar-output', stdout);
+    console.log('Executed Java with jarPath1');
 
     console.log(`Jar Path: ${jarPath2}`);
     await exec.exec('java', ['-jar', jarPath2, name]);
+    console.log('Executed Java with jarPath2');
     //core.setOutput('jar-output', stdout);
 
   } catch (error) {
