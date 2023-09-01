dc242916bc00e2118c240524521b948af7835714
AleksandraAleksandrova <a.aleksandrova2004@gmail.com>
2023-09-01 12:40:10 +0300

    debug 3

--- main.js
+++ main.js
@@ -1,17 +1,13 @@
 const core = require('@actions/core');
 const exec = require('@actions/exec');
 const path = require('path');
-const fs = require('fs');
 const workingDir = process.cwd();
-const filesInDir = fs.readdirSync(workingDir);
-
 console.log(`Working Directory: ${workingDir}`);
-console.log(`Files in Directory: ${filesInDir.join(', ')}`);
 
 async function run() {
   try {
     const name =  "Anna" //core.getInput('name');
-    //const jarPath = './Greet.jar';
+    const jarPath = './Greet.jar';
     const jarPath1 = path.join(process.env.GITHUB_WORKSPACE, 'Greet.jar');
     const jarPath2 = path.join(__dirname, 'Greet.jar');
 
@@ -25,6 +21,10 @@ async function run() {
       },
     };
     */
+
+    console.log(`Jar Path: ${jarPath}`);
+    await exec.exec('java', ['-jar', jarPath, name]);
+    //core.setOutput('jar-output', stdout);
 
     console.log(`Jar Path: ${jarPath1}`);
     await exec.exec('java', ['-jar', jarPath1, name]);
