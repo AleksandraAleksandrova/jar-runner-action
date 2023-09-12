39ce3ce92bc89453f7a095e8516266eb3af7a52d
AleksandraAleksandrova <a.aleksandrova2004@gmail.com>
2023-09-12 13:48:46 +0300

    dummy data

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 const core = require('@actions/core');
 const exec = require('@actions/exec');
-const path = require('path');
-const workingDir = process.cwd();
 
 async function run() {
   try {
     const name = 'Anna';
     const jarPath = './Greet.jar'
 
+    const name2 = core.getInput('name');
+    console.log(`Hello ${name2}!`);
+
     await exec.exec('java', ['-jar', jarPath, name]);
 
-    //core.setOutput('jar-output', stdout);
+    core.setOutput('jar-output', 'Success');
 
   } catch (error) {
     core.setFailed(`Action failed with error: ${error.message}`);
