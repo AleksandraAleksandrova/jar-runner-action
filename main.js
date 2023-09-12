5fd53aaa4aa8d1751133fdbef4b64de3430b2d73
AleksandraAleksandrova <a.aleksandrova2004@gmail.com>
2023-09-12 14:58:18 +0300

    try using path

--- main.js
+++ main.js
@@ -1,13 +1,24 @@
 const core = require('@actions/core');
 const exec = require('@actions/exec');
+const path = require('path');
 
 async function run() {
   try {
-    const jarPath = './Greet.jar'
     const name = core.getInput('name');
+    const jarPath = path.join(__dirname, 'Greet.jar');
 
-    await exec.exec('java', ['-jar', jarPath, name]);
-    core.setOutput('jar-output', 'Success');
+    let stdout = '';
+
+    const options = {
+      listeners: {
+        stdout: (data) => {
+          stdout += data.toString();
+        },
+      },
+    };
+
+    await exec.exec('java', ['-jar', jarPath, name], options);
+    core.setOutput('jar-output', stdout);
 
   } catch (error) {
     core.setFailed(`Action failed with error: ${error.message}`);
