716a5e1d33ad096ce23cb6733e410490f0e9399e
AleksandraAleksandrova <a.aleksandrova2004@gmail.com>
2023-09-12 11:44:07 +0300

    try again from the beginnig

--- main.js
+++ main.js
@@ -5,28 +5,11 @@ const workingDir = process.cwd();
 
 async function run() {
   try {
-    const name =  "Anna" //core.getInput('name');
-    const jarPath1 = path.join(process.env.GITHUB_WORKSPACE, 'Greet.jar');
-    const jarPath2 = path.join(__dirname, 'Greet.jar');
+    const name = 'Anna';
+    const jarPath = './Greet.jar'
 
-    /*
-    let stdout = '';
-    const options = {
-      listeners: {
-        stdout: (data) => {
-          stdout += data.toString();
-        },
-      },
-    };
-    */
+    await exec.exec('java', ['-jar', jarPath, name]);
 
-    console.log(`Jar Path: ${jarPath1}`);
-    await exec.exec('java', ['-jar', jarPath1, name]);
-    console.log('Executed Java with jarPath1');
-
-    console.log(`Jar Path: ${jarPath2}`);
-    await exec.exec('java', ['-jar', jarPath2, name]);
-    console.log('Executed Java with jarPath2');
     //core.setOutput('jar-output', stdout);
 
   } catch (error) {
